12d439166ca8b09018178332b5b59de996b9a66a
pranav20 <pranav20111998@gmail.com>
2019-10-18 12:25:28 +0800

    added tab navigation

---
+++ frontend/cforg/src/MainApp/Main.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import {AppBar, Tab, Tabs} from "@material-ui/core";
+import TabPanel from './TabPanel';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Donors" {...a11yProps(0)} />
+          <Tab label="Events" {...a11yProps(1)} />
+          <Tab label="Tags" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </div>
+  );
+}
